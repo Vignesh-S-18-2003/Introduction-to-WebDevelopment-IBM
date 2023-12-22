b0d4e09726dc3fdc1709e263d7adf546be155cf4
Vignesh S <116181639+Vignesh-S-18-2003@users.noreply.github.com>
2023-12-22 10:31:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const homeIcon = document.getElementById('home-icon');
+
+    homeIcon.addEventListener('click', function () {
+        alert('Home icon clicked!');
+    });
+});
